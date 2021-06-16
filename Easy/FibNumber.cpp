7ccff76a4d5f60f341ccf638dd9c99665078e6f8
Shi Tao Luo <sluo5991@gmail.com>
2021-06-16 18:21:29 -0400

    Update FibNumber.cpp

--- Easy/FibNumber.cpp
+++ Easy/FibNumber.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
+        //first two number
         if (n <= 1) {
             return  n;
         }
@@ -10,7 +11,36 @@ public:
         int current = 0;
         int previous = 1;
         int previous2 = 1;
+        //loop starting at 3 
         for (int i = 3; i <= n; i++) {
+            //iterativly calculate fib(n)
+            /*
+                ex) fib(6)
+                    curr = 0
+                    prev = 1
+                    prev2 = 1
+                    ----------
+                    i = 3
+                    curr = 2
+                    prev2 = 1
+                    prev = 2
+                    ----------
+                    i = 4
+                    curr = 3
+                    prev2 = 2
+                    prev = 3
+                    ----------
+                    i = 5
+                    curr = 5
+                    prev2 = 3
+                    prev = 5
+                    ----------
+                    i = 6
+                    curr = 8
+                    prev2 = 5
+                    prev = 8
+                    return curr = 8
+            */
             current = previous + previous2;
             previous2 = previous;
             previous = current;

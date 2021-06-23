822a6dd08a75034380807a3e008bf7dacf185493
Shi Tao Luo <sluo5991@gmail.com>
2021-06-23 08:38:21 -0400

    Update secondHighest.cpp

--- Easy/secondHighest.cpp
+++ Easy/secondHighest.cpp
@@ -17,26 +17,48 @@ Explanation: The digits that appear in s are [1]. There is no second largest dig
 class Solution {
 public:
     int secondHighest(string s) {
-        
+        //Time: O(N)
+        //Space: O(N)
         //edge case: 
         if (s.length() <= 1) {
             return -1;
         }
-        
         set<int> res;
-        
         //add digits to set
         for (char c : s) {
             if (isdigit(c)) {
                 res.insert(c-'0');
             }
         }
-        
         //edge case:
         if (res.size() < 2) return -1;
-        
         //iterate to the second index in set;
         auto resIt = res.rbegin();
         return *(++resIt);
+        
+        //Time: O(N)
+        //Space: O(1)
+        int n = s.length();
+        int max = -1, secondMax = -1;
+        
+        //loop through string s
+        for (int i = 0; i < n; i++) {
+            //if number is a integer, set max/second max
+            if (s[i] >= '0' && s[i] <= '9') {
+                //problem
+                // int nums = s[i];
+                int nums = s[i] - '0';
+                if (nums > max) {
+                    //dfa12321afd
+                        //secondMax = -1
+                        //max = 1
+                    secondMax = max;
+                    max = nums;
+                } else if (nums < max && nums > secondMax) {
+                    secondMax = max;
+                }
+            }
+        }
+        return secondMax;
     }
 };

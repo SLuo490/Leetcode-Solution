98d497125e043733bd011fb739297cb0f90ae2d3
Shi Tao Luo <sluo5991@gmail.com>
2021-06-27 08:58:12 -0400

    Update MergeTwoSortedList.cpp

--- Easy/MergeTwoSortedList.cpp
+++ Easy/MergeTwoSortedList.cpp
@@ -1,35 +1,44 @@
+/*
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Example 1:
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: l1 = [], l2 = []
+Output: []
+
+Example 3:
+Input: l1 = [], l2 = [0]
+Output: [0]
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        //if list 1 is empty, return list 2
-        if (!l1)
-        {
-            return l2;
-        }
-        //if list 2 is empty, return list 1
-        else if (!l2)
-        {
-            return l1;
-        }
-        //if both is empty, return null
-        else if (!l1 && !l2)
-        {
-            return NULL;
-        }
-        //recursive
-        else if (l1->val < l2->val)
-        {
-            //recursive l1
+        //Edge Case
+        if (!l1) return l2;
+        if (!l2) return l1;
+        if (!l1 && !l2) return nullptr;
+        
+        //Recursion
+        if (l1->val < l2->val) {
             l1->next = mergeTwoLists(l1->next, l2);
             return l1;
-        }
-        else 
-        {
-            //recursive l2
+        } else {
             l2->next = mergeTwoLists(l1, l2->next);
             return l2;
-            
         }
-        
     }
 };

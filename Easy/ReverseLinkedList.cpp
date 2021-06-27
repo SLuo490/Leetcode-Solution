ed9f9cd8b7f05f14a9d9fe7edcf9d854c5cf910f
Shi Tao Luo <sluo5991@gmail.com>
2021-06-27 09:03:24 -0400

    Update ReverseLinkedList.cpp

--- Easy/ReverseLinkedList.cpp
+++ Easy/ReverseLinkedList.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
+           //Iterative
+//         ListNode* prev = nullptr;
+//         ListNode* curr = head;
         
-        //traverse the list
-        while (curr != nullptr)
-        {
-            //create a temp node that move to the next node
-            ListNode* tempNode = curr->next;
-            //set curr.next to prev pointer
-            curr->next = prev;
-            //set prev to curr
-            prev = curr;
-            //set curr to temp
-            curr = tempNode;
+//         //traverse the list
+//         while (curr != nullptr)
+//         {
+//             //create a temp node that move to the next node
+//             ListNode* tempNode = curr->next;
+//             //set curr.next to prev pointer
+//             curr->next = prev;
+//             //set prev to curr
+//             prev = curr;
+//             //set curr to temp
+//             curr = tempNode;
+//         }
+//         return prev;
+        
+        //Recursion
+        if (head -- mullptr || head->next == nullptr) {
+            return head;
         }
-        return prev;
+        ListNode *res = reverseList(head->next);
+        // Or
+        // ListNode *q = head->next;
+        // q->next = head;
+        // head->next = nullptr;
+        head->next->next = head;
+        head->next = nullptr;
+        
+        return res;
     }
 };

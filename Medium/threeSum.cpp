94da171b333b194073c6d7df472d9bb2361ee92f
Shi Tao Luo <sluo5991@gmail.com>
2021-08-31 08:51:36 -0400

    Create threeSum.cpp

---
+++ Medium/threeSum.cpp
@@ -0,0 +1,46 @@
+/*
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+
+Example 2:
+Input: nums = []
+Output: []
+
+Example 3:
+Input: nums = [0]
+Output: []
+*/
+
+class Solution {
+public:
+    //loop through the nums and find negative number
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for (int i = 0; i < nums.size() && nums[i] <= 0; ++i)
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSumII(nums, i, res);
+            }
+        return res;
+    }
+    //find pair that adds up to 0
+    void twoSumII(vector<int>& nums, int i, vector<vector<int>> &res) {
+        int lo = i + 1, hi = nums.size() - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0) {
+                lo++;
+            } else if (sum > 0) {
+                hi--;
+            } else {
+                res.push_back({ nums[i], nums[lo++], nums[hi--] });
+                while (lo < hi && nums[lo] == nums[lo - 1])
+                    lo++;
+            }
+        }
+    }
+};

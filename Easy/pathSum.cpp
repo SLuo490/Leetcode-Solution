ebbbb5abd4f4fe404b75de3ed96de16ea18f5f93
Shi Tao Luo <sluo5991@gmail.com>
2021-09-28 08:27:30 -0400

    Create pathSum.cpp

---
+++ Easy/pathSum.cpp
@@ -0,0 +1,45 @@
+/*
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+A leaf is a node with no children.
+
+Example 1:
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+
+Example 2:
+Input: root = [1,2,3], targetSum = 5
+Output: false
+
+Example 3:
+Input: root = [1,2], targetSum = 0
+Output: false
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == nullptr) {
+            return false;
+        }
+        
+        //if the root-> val is equal to sum and is leaf then we found a path
+        if (root->val == sum && root->left == nullptr && root->right == nullptr) {
+            return true;
+        }
+        
+        //recursive call left and right sub tree
+        return hasPathSum (root->left, sum - root->val) || hasPathSum (root->right, sum - root->val);
+    }
+};

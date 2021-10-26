5a76e9566e09fd3ff527756c74abb5662f2cd56c
Shi Tao Luo <sluo5991@gmail.com>
2021-10-26 08:44:36 -0400

    Create findMissingPositive.cpp

---
+++ _Hard/findMissingPositive.cpp
@@ -0,0 +1,54 @@
+/*
+Given an unsorted integer array nums, return the smallest missing positive integer.
+You must implement an algorithm that runs in O(n) time and uses constant extra space.
+Example 1:
+Input: nums = [1,2,0]
+Output: 3
+Example 2:
+Input: nums = [3,4,-1,1]
+Output: 2
+Example 3:
+Input: nums = [7,8,9,11,12]
+Output: 1
+*/
+
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size(); 
+        
+        //cyclic sort
+        for (int i = 0; i < n; i++) {
+            //edge case: 
+            //Swap if num is between 0 - n && num[i] != nums[nums[i] - 1] -> ([1,1])
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]); 
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1; 
+            }
+        }
+        
+        //if number is sorted, return the size + 1 [https://leetcode.com/problems/missing-number/]
+        return n + 1;
+    }
+};
+
+/*
+[ 1, 2, -3, 4, 5]
+  0  1  2  3  4  
+  
+  
+[0,  1, 2, 3, -2]
+ 0   1  2  3  4
+[4]
+[7,8,9,11,12]
+ 0 1 2  3  4
+* Edge Cases To Skip
+1) Last element >= nums.size() -> skip
+2) Out of bound element
+*/

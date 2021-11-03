fbd1b5a3f01e15820f294bb88976429c59abbf17
Shi Tao Luo <sluo5991@gmail.com>
2021-11-03 08:35:01 -0400

    Create rotateArray.cpp

---
+++ Medium/rotateArray.cpp
@@ -0,0 +1,68 @@
+/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+*/
+
+
+class Solution {
+public:
+    //O(n) Time and Space
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size(); 
+        vector<int> res (n); 
+        
+        for (int i = 0; i < n; i++) {
+            res[(i + k) % n] = nums[i]; //rotate array (MATH)
+        }
+        nums = res; 
+    }
+};
+
+/*
+[1,2,3,4,5]
+ -           
+
+k = 3, n = 5
+(i + k) % n = 0 + 3 % 5 = 3
+
+res[3] = nums[0]
+
+[0,0,0,1,0]
+
+-------------------------------
+[1,2,3,4,5]
+
+k = 3, n = 5
+(i + k) % n = 1 + 3 % 5 = 4
+
+res[4] = nums[1]
+
+[0,0,0,1,2]
+
+-------------------------------
+[1,2,3,4,5]
+
+k = 3, n = 5
+(i + k) % n = 2 + 3 % 5 = 0
+
+res[4] = nums[2]
+
+[3,0,0,1,2]
+
+...
+
+*/

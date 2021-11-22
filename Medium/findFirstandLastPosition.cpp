7f601e0e32f597bc92fad61ace32f2c89f6da8cd
Shi Tao Luo <sluo5991@gmail.com>
2021-11-22 08:54:33 -0500

    Update findFirstandLastPosition.cpp

--- Medium/findFirstandLastPosition.cpp
+++ Medium/findFirstandLastPosition.cpp
@@ -46,3 +46,57 @@ public:
         return {left, right}; 
     }
 };
+
+// Easier to Read Solution
+
+class Solution {
+public:
+    int findLeftMost (vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1; 
+        int leftMost = -1; 
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] < target) {
+                lo = mid + 1; 
+            } else if (nums[mid] > target) {
+                hi = mid - 1; 
+            } else {
+                leftMost = mid; 
+                hi = mid - 1; 
+            }
+        }
+        return leftMost; 
+    }
+    
+    int findRightMost (vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1; 
+        int rightMost = -1; 
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (nums[mid] < target) {
+                lo = mid + 1; 
+            } else if (nums[mid] > target) {
+                hi = mid - 1; 
+            } else {
+                rightMost = mid; 
+                lo = mid + 1; 
+            }
+        }
+        return rightMost; 
+    }
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int leftMost = findLeftMost(nums, target); 
+        //edge case: if leftMost is -1, then we cannot find a position
+        if (leftMost == - 1) {
+            return {-1, -1}; 
+        }
+        int rightMost = findRightMost(nums, target);  
+        
+        return {leftMost, rightMost}; 
+    }
+};

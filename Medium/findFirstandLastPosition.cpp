1819bb5d90fbd942a261e3847b913097f42426b2
Shi Tao Luo <sluo5991@gmail.com>
2021-11-22 08:48:38 -0500

    Create findFirstandLastPosition.cpp

---
+++ Medium/findFirstandLastPosition.cpp
@@ -0,0 +1,48 @@
+/*
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+
+Example 3:
+Input: nums = [], target = 0
+Output: [-1,-1]
+*/
+
+class Solution {
+public:
+    int binarySearch (vector<int>& nums, int target, bool leftBias) {
+        int lo = 0, hi = nums.size() - 1; 
+        int i = -1; 
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2; 
+            if (nums[mid] < target) {
+                lo = mid + 1; 
+            } else if (nums[mid] > target) {
+                hi = mid - 1; 
+            } else {
+                // find the left most and right most
+                i = mid; 
+                if (leftBias) {
+                    hi = mid - 1; 
+                } else {
+                    lo = mid + 1; 
+                }
+            }
+        }
+        return i; 
+    }
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int left = binarySearch(nums, target, true); 
+        int right = binarySearch(nums, target, false); 
+        return {left, right}; 
+    }
+};

348e20afe99ca59e324b8c36252105850a726427
SLuo5991 <sluo5991@gmail.com>
2022-08-02 11:21:29 -0400

    solve valid sudoku

--- validSudoku.cpp
+++ validSudoku.cpp
@@ -59,31 +59,35 @@ bool isValidCol (vector<vector<char>>& board) {
 
 // function to check all 3x3 squares in board
 bool isValidSquare(vector<vector<char>>& board) {
-    for (int i = 0; i < board.size(); i++) {
+    for (int row = 0; row < board.size(); row+=3) {
         // traverse to 3
-        for (int j = 0; j < board.size(); j++) {
+        for (int col = 0; col < board.size(); col+=3) {
+            set<char> seen; 
+            for (int rowx = row; rowx < row + 3; rowx++) {
+                for (int coly = col; coly < row + 3; coly++) {
+                    char currentChar = board[rowx][coly]; 
+                    if (seen.find(currentChar) != seen.end()) {
+                        return false; 
+                    }
 
+                    if (currentChar == ',' || currentChar == '.') {
+                        continue; 
+                    } else {
+                        seen.insert(currentChar); 
+                    }
+                }
+            }
+            seen.clear(); 
         }
     }
-    return false; 
+    return true; 
 }
-
-/*
-      0 1 2 3 4 5 
-    0 0 0 0 0 0 0
-    1 0 0 0 0 0 0
-    2 0 0 0 0 0 0
-    3 0 0 0 0 0 0
-    4 0 0 0 0 0 0
-    5 0 0 0 0 0 0
-*/
-
 
 int main() {
     vector<vector<char>> board {
          {'5','3','.','.','7','.','.','.','.'},
-         {'6','.','.','1','9','5','.','.','.'},
-         {'.','9','8','.','.','.','.','6','.'},
+         {'6','2','.','1','9','5','.','.','.'},
+         {'.','9','9','.','.','.','.','6','.'},
          {'8','.','.','.','6','.','.','.','3'},
          {'4','.','.','8','.','3','.','.','1'},
          {'7','.','.','.','2','.','.','.','6'},
@@ -92,7 +96,20 @@ int main() {
          {'.','.','.','.','8','.','.','7','9'}
     }; 
 
-    bool res = isValidSquare(board); 
+    vector<vector<char>> board2 = 
+        {
+            {'.','.','.','.','5','.','.','1','.'},
+            {'.','4','.','3','.','.','.','.','.'},
+            {'.','.','.','.','.','3','.','.','1'},
+            {'8','.','.','.','.','.','.','2','.'},
+            {'.','.','2','.','7','.','.','.','.'},
+            {'.','1','5','.','.','.','.','.','.'},
+            {'.','.','.','.','.','2','.','.','.'},
+            {'.','2','.','9','.','.','.','.','.'},
+            {'.','.','4','.','.','.','.','.','.'}
+        }; 
+
+    bool res = isValidSudoku(board2); 
 
     if (res == true) {
         cout << "True" << endl; 

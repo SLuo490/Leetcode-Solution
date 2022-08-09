5783385083ff0fad24d04eec2a3258f639e6d558
SLuo5991 <sluo5991@gmail.com>
2022-08-09 07:21:50 -0400

    add comment

--- longestConsecutiveSequence.cpp
+++ longestConsecutiveSequence.cpp
@@ -15,6 +15,23 @@ using namespace std;
     Output: 9
 */
 
+/*
+    Pseudo code
+    
+    create a set and all nums to it
+    initialize result 
+    traverse through the set
+        get the current number in set
+        check if the previous number is in set if it is, then we need to continue
+            * because start of sequence, there are no previous number
+        initialize current length to 1
+        do a while loop until number + 1 is not in set anymore
+            increment current length
+            increment the number
+        calculate the max sequence
+    return the max sequence
+*/
+
 int longestConsecutive(vector<int>& nums) {
     // add numbers to a set; removing duplicate numbers
     unordered_set<int> s;
@@ -26,6 +43,8 @@ int longestConsecutive(vector<int>& nums) {
     int res = 0; 
     for (auto it : s) {
         int num = it; 
+        // why do you continue if the previous number is in set?
+        // to find the start of the sequence, we need to check if the previous number is in set or not
         if (s.find(num - 1) != s.end()) continue; 
         int currLength = 1; 
         while (s.find(num + 1) != s.end()) {

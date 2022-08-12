78689cf53a7dbfbe99e3fc13c1cfa7d23f2efec3
SLuo5991 <sluo5991@gmail.com>
2022-08-12 08:32:24 -0400

    add brute force method

--- trappingRainWater.cpp
+++ trappingRainWater.cpp
@@ -1,7 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
-int trap (vector<int>& height) {
+// Brute Force
+// Time: O(N^2)
+// Space: O(1)
+int trap(vector<int>& height) {
+    int res = 0; 
+    for (int i = 0; i < height.size(); i++) {
+        int leftMax = 0, rightMax = 0; 
+        // find the left max
+        for (int j = 0; j <= i; j++) {
+            leftMax = max(leftMax, height[j]); 
+        }
+
+        // find the right max
+        for (int k = i; k < height.size(); k++) {
+            rightMax = max(rightMax, height[k]); 
+        }
+
+        res += min(leftMax, rightMax) - height[i]; 
+    }
+    return res; 
+}
+
+
+// Time : O(N)
+// Space: O(1)
+int trap2 (vector<int>& height) {
     int left = 0, right = height.size() - 1; 
     int leftMax = 0, rightMax = 0; 
     int res = 0; 

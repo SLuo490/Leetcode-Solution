9ace2a9220dbff67b5218a907ce872742d9c7083
SLuo5991 <sluo5991@gmail.com>
2022-08-28 07:47:19 -0400

    optimized license key

--- License Key Formatting.cpp
+++ License Key Formatting.cpp
@@ -21,6 +21,20 @@ Explanation: The string s has been split into three parts, each part has 2 chara
  
 */
 
+// Optimal Solution
+// Use res.size() % (k + 1) == k
+string licenseKeyFormatting(string S, int K) {
+    string res;
+    for (auto i = S.rbegin(); i < S.rend(); i++)
+    if (*i != '-') { // ignore '-' in original string
+        if (res.size()%(K+1) == K) res += '-'; // every (K+1)th char is '-' from tail
+        res += toupper(*i);
+    }
+    
+    reverse(res.begin(), res.end());
+    return res;
+}
+
 string licenseKeyFormatting(string s, int k) {
     stack<char> sChars; 
     for (char ch : s) {

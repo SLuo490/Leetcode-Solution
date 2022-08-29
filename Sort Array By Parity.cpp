38248a43813496433e14b71ed82a37a527bc711a
SLuo5991 <sluo5991@gmail.com>
2022-08-29 09:07:45 -0400

    optimize sort array by parity

--- Sort Array By Parity.cpp
+++ Sort Array By Parity.cpp
@@ -37,6 +37,41 @@ public:
         }
         return nums; 
     }
+
+    // Optimal: Swapping In place
+    // Time: O(N), space: O(1)
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        for (int i = 0, j = 0; j < nums.size(); j++) {
+            if (nums[j] % 2 == 0) swap(nums[i++], nums[j]); 
+        }
+        return nums; 
+    }
+
+    /*
+        [3, 1, 2, 4]
+         -
+         -
+        
+        nums[j] % 2 != 0, increment j
+
+        [3, 1, 2, 4]
+         -
+            -
+        
+        nums[j] % 2 != 0, increment j
+
+
+        [2, 1, 3, 4]
+            -
+                  -
+        nums[j] % 2 == 0, swap j and j, and increment i
+
+        [2, 4, 3, 1]
+            -
+                  -
+        nums[j] % 2 == 0, swap j and j, and increment i
+        end of loop
+    */
 };
 
 int main()

bb8f1b1e569ec5edd36d57eebf1fc4ce9e75c1cf
SLuo5991 <sluo5991@gmail.com>
2022-09-18 08:20:26 -0400

    add number of zero-filled subarrays

---
+++ Number of Zero-Filled Subarrays.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Given an integer array nums, return the number of subarrays filled with 0.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+Input: nums = [1,3,0,0,2,0,0,4]
+Output: 6
+Explanation: 
+There are 4 occurrences of [0] as a subarray.
+There are 2 occurrences of [0,0] as a subarray.
+There is no occurrence of a subarray with a size more than 2 filled with 0. Therefore, we return 6.
+
+Example 2:
+Input: nums = [0,0,0,2,0,0]
+Output: 9
+Explanation:
+There are 5 occurrences of [0] as a subarray.
+There are 3 occurrences of [0,0] as a subarray.
+There is 1 occurrence of [0,0,0] as a subarray.
+There is no occurrence of a subarray with a size more than 3 filled with 0. Therefore, we return 9.
+
+Example 3:
+Input: nums = [2,10,2019]
+Output: 0
+Explanation: There is no subarray filled with 0. Therefore, we return 0.
+*/
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long res = 0; 
+        int windowStart = 0; 
+        
+        for (int windowEnd = 0; windowEnd < nums.size(); windowEnd++) {
+            // slide window if the current element is not a 0
+            if (nums[windowEnd] != 0) {
+                windowStart = windowEnd + 1; 
+            }
+            // add the length of the window of subarrays containing 0
+            res += windowEnd - windowStart + 1; 
+        }
+        return res; 
+    }
+};
+
+/*
+     -
+    [0,0,0,2,0,0]
+         -
+     
+     i = 5
+     j = 4
+     
+     res = i - j + 1 --> 9
+*/
+
+int main()
+{
+  return 0;
+}

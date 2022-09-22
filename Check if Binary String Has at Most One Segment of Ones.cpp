5537c4d2b70c10a577a04d085416aa94c2010807
Shi Tao Luo <sluo5991@gmail.com>
2022-09-22 17:51:59 -0400

    solve check if binary string has at most one seg of ones

---
+++ Check if Binary String Has at Most One Segment of Ones.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+
+Example 1:
+Input: s = "1001"
+Output: false
+Explanation: The ones do not form a contiguous segment.
+
+Example 2:
+Input: s = "110"
+Output: true
+*/
+
+class Solution {
+public:
+  // Time: O(N)
+  // Space: O(1)
+  bool checkOnesSegment(string s) {
+      if (s.length() == 1) return true;  
+      
+      int numSeg = 0; 
+      
+      for (int i = 0; i < s.length();) {
+          int j = i + 1; 
+          if (s[i] == '1') {
+              while (s[j] == '1') {
+                  j++; 
+              }
+              numSeg++; 
+          }
+          i = j; 
+      }
+      
+      return numSeg == 1; 
+  }
+};
+
+int main()
+{
+  return 0;
+}

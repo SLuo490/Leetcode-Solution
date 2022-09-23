7c305405dab6de93e36f5d8dbb54356d86f71e88
SLuo5991 <sluo5991@gmail.com>
2022-09-23 19:56:12 -0400

    optimized solution

--- Easy/Time Needed To Buy Tickets.cpp
+++ Easy/Time Needed To Buy Tickets.cpp
@@ -46,6 +46,14 @@ public:
         }
         return res; 
     }
+
+    // Optimized Solution
+    int timeRequiredToBuy(vector<int>& t, int k) {
+      int res = 0, sz = t.size(), cap = t[k];
+      for (int i = 0; i < sz; ++i)
+          res += min(cap - (i > k), t[i]);
+      return res;
+    }
 };
 
 

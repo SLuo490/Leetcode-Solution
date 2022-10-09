b10536f3d7347a1058b77cd0d1bf9cb98cfb0deb
SLuo5991 <sluo5991@gmail.com>
2022-10-09 08:51:31 -0400

    push add lowest common ancestor of binary search tree

---
+++ Medium/Lowest Common Ancestor of Binary Search Tree.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {        
+        int parentVal = root->val; 
+        int pVal = p->val; 
+        int qVal = q->val; 
+        
+        // if p and q's value is less than the root value, traverse to the right tree, else left tree
+        if (pVal > parentVal && qVal > parentVal) {
+            return lowestCommonAncestor(root->right, p, q); 
+        } else if (pVal < parentVal && qVal < parentVal) {
+            return lowestCommonAncestor(root->left, p, q); 
+        } else {
+            return root;
+        }
+    }
+};
+
+int main()
+{
+  return 0;
+}

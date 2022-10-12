52a2a4f45065b558706cc29b0587b10319f69acd
SLuo5991 <sluo5991@gmail.com>
2022-10-12 09:14:56 -0400

    added min value to get positive step by step sum

---
+++ Easy/min value to get positive step by step sum.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Given an array of integers nums, you start with an initial positive value startValue.
+In each iteration, you calculate the step by step sum of startValue plus elements in nums (from left to right).
+Return the minimum positive value of startValue such that the step by step sum is never less than 1.
+
+Example 1:
+Input: nums = [-3,2,-3,4,2]
+Output: 5
+Explanation: If you choose startValue = 4, in the third iteration your step by step sum is less than 1.
+step by step sum
+startValue = 4 | startValue = 5 | nums
+  (4 -3 ) = 1  | (5 -3 ) = 2    |  -3
+  (1 +2 ) = 3  | (2 +2 ) = 4    |   2
+  (3 -3 ) = 0  | (4 -3 ) = 1    |  -3
+  (0 +4 ) = 4  | (1 +4 ) = 5    |   4
+  (4 +2 ) = 6  | (5 +2 ) = 7    |   2
+
+Example 2:
+Input: nums = [1,2]
+Output: 1
+Explanation: Minimum start value should be positive. 
+*/
+
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        int startVal = 1; 
+        
+        while (true) {
+            int total = startVal; 
+            bool valid = true; 
+            
+            for (int num : nums) {
+                total += num; 
+                
+                // if total is less than one set valid to false and break
+                if (total < 1) {
+                    valid = false; 
+                    break; 
+                }
+            }
+            
+            // if valid is true return the start value else increment start value for next iteration
+            if (valid) {
+                return startVal; 
+            } else {
+                startVal++; 
+            }
+        }
+        return -1; 
+    }
+};
+
+int main()
+{
+  return 0;
+}
